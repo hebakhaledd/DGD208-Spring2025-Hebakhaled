c5999a83830d4c04d5ee13df84ba49ba9ad4ea82
Hebakhaledd <kheba7355@gmail.com>
2025-05-02 21:55:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+namespace DGD208_Spring2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, DGD208 Spring 2025!");
+        }
+    }
+}
+< Project Sdk = "Microsoft.NET.Sdk" >
+
+  < PropertyGroup >
+    < OutputType > Exe </ OutputType >
+    < TargetFramework > net6.0 </ TargetFramework >
+    < Nullable > enable </ Nullable >
+    < ImplicitUsings > enable </ ImplicitUsings >
+  </ PropertyGroup >
+
+</ Project >
